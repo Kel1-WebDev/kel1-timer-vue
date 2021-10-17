ed77177b84394792bee3c05787748ef16fa89ba2
reyrizki <reynaldirizki97.rr@gmail.com>
2021-10-17 23:07:49 +0700

    feature(Front-End): Init Vue.js
    - Create Vue.js Project
    - Refactor web component to Vue.js component

---
+++ front-end/src/components/StopwatchItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <h1 name class="name dark-blue">WebDev Teori</h1>
+    <img delete class="del-btn" src="../assets/delete.svg" />
+  </div>
+  <div>
+    <div class="blabla">
+      <p time class="time">00:00:00</p>
+      <div class="container">
+        <img start class="timer-btn" src="../assets/play.svg" />
+        <img stop class="timer-btn dark-blue" src="../assets/stop.svg" />
+      </div>
+    </div>
+  </div>
+  <div>
+    <div history class="history">
+      <button showHistory class="accordion"><span> ▶ </span> History</button>
+      <div historyList class="history-list"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StopwatchItem",
+};
+</script>
+
+<style scoped>
+@import url("../styles/timer.css");
+@import url("../styles/history.css");
+</style>
---
+++ front-end/src/components/StopwatchList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="center-content">
+    <h1 id="title">cronômetro</h1>
+  </div>
+  <div class="row">
+    <div class="fixed-list">
+      <div id="container">
+        <stopwatch-item />
+      </div>
+    </div>
+    <div>
+      <div id="form">
+        <form>
+          <h2>CREATE NEW STOPWATCH</h2>
+          <label for="stopwatch-name">Insert a new stopwatch's label</label>
+          <input
+            type="text"
+            id="stopwatch-name"
+            name="stopwatch-name"
+            placeholder="Enter the label"
+          />
+          <input id="submit" type="button" value="Create Stopwatch" />
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import StopwatchItem from "./StopwatchItem.vue";
+
+export default {
+  name: "StopwatchList",
+  components: {
+    StopwatchItem,
+  },
+};
+</script>
+
+<style scoped>
+@import url("../styles/form.css");
+</style>
