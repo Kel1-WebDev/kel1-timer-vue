cac5ede9c25ede26eb6e96a7a25a2268f1517f02
Icad <irsyadmuhammad.020201@gmail.com>
2021-10-18 19:04:46 +0700

    ch(timer);Solve vue multiple template root

--- front-end/src/components/StopwatchItem.vue
+++ front-end/src/components/StopwatchItem.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="container">
-    <h1 name class="name dark-blue">WebDev Teori</h1>
-    <img delete class="del-btn" src="../assets/delete.svg" />
-  </div>
   <div>
-    <div class="blabla">
-      <p time class="time">00:00:00</p>
-      <div class="container">
-        <img start class="timer-btn" src="../assets/play.svg" />
-        <img stop class="timer-btn dark-blue" src="../assets/stop.svg" />
+    <div class="container">
+      <h1 name class="name dark-blue">WebDev Teori</h1>
+      <img delete class="del-btn" src="../assets/delete.svg" />
+    </div>
+    <div>
+      <div class="blabla">
+        <p time class="time">00:00:00</p>
+        <div class="container">
+          <img start class="timer-btn" src="../assets/play.svg" />
+          <img stop class="timer-btn dark-blue" src="../assets/stop.svg" />
+        </div>
       </div>
     </div>
-  </div>
-  <div>
-    <div history class="history">
-      <button showHistory class="accordion"><span> ▶ </span> History</button>
-      <div historyList class="history-list"></div>
+    <div>
+      <div history class="history">
+        <button showHistory class="accordion"><span> ▶ </span> History</button>
+        <div historyList class="history-list"></div>
+      </div>
     </div>
   </div>
 </template>
--- front-end/src/components/StopwatchList.vue
+++ front-end/src/components/StopwatchList.vue
@@ -1,26 +1,28 @@
 <template>
-  <div class="center-content">
-    <h1 id="title">cronômetro</h1>
-  </div>
-  <div class="row">
-    <div class="fixed-list">
-      <div id="container">
-        <stopwatch-item />
-      </div>
+  <div>
+    <div class="center-content">
+      <h1 id="title">cronômetro</h1>
     </div>
-    <div>
-      <div id="form">
-        <form>
-          <h2>CREATE NEW STOPWATCH</h2>
-          <label for="stopwatch-name">Insert a new stopwatch's label</label>
-          <input
-            type="text"
-            id="stopwatch-name"
-            name="stopwatch-name"
-            placeholder="Enter the label"
-          />
-          <input id="submit" type="button" value="Create Stopwatch" />
-        </form>
+    <div class="row">
+      <div class="fixed-list">
+        <div id="container">
+          <stopwatch-item />
+        </div>
+      </div>
+      <div>
+        <div id="form">
+          <form>
+            <h2>CREATE NEW STOPWATCH</h2>
+            <label for="stopwatch-name">Insert a new stopwatch's label</label>
+            <input
+              type="text"
+              id="stopwatch-name"
+              name="stopwatch-name"
+              placeholder="Enter the label"
+            />
+            <input id="submit" type="button" value="Create Stopwatch" />
+          </form>
+        </div>
       </div>
     </div>
   </div>
