9738a73f816a00d7cb59d7d29a695ee4713347b6
reyrizki <reynaldirizki97.rr@gmail.com>
2021-10-21 22:14:50 +0700

    chore(Timer): Change Start Button

--- front-end/src/components/StopwatchItem.vue
+++ front-end/src/components/StopwatchItem.vue
@@ -8,8 +8,13 @@
       <div class="blabla">
         <p time class="time">{{ formatTime(time) }}</p>
         <div class="container">
-          <img start class="timer-btn" src="../assets/play.svg" @click="start()"/>
-          <img stop class="timer-btn dark-blue" src="../assets/stop.svg" @click="stop()"/>
+          <img start class="timer-btn" v-bind:src="startButton" @click="start()" />
+          <img
+            stop
+            class="timer-btn dark-blue"
+            src="../assets/stop.svg"
+            @click="stop()"
+          />
         </div>
       </div>
     </div>
@@ -29,13 +34,13 @@ export default {
     return {
       time: 0,
       state: "stop",
-      interval: null
-    }
+      interval: null,
+      startButton: require("../assets/play.svg"),
+    };
   },
   methods: {
     insertZero(time) {
-      if (time < 10)
-        return "0" + time;
+      if (time < 10) return "0" + time;
       return time;
     },
     formatTime(second) {
@@ -48,26 +53,35 @@ export default {
       const minute = Math.floor(second / divisor);
       second = second % divisor;
 
-      return this.insertZero(hour) + ":" + this.insertZero(minute) + ":" + this.insertZero(second);
+      return (
+        this.insertZero(hour) +
+        ":" +
+        this.insertZero(minute) +
+        ":" +
+        this.insertZero(second)
+      );
     },
     incrementTime() {
       this.time++;
     },
     start() {
-      if ((this.state === "pause") || (this.state === "stop")) {
+      if (this.state === "pause" || this.state === "stop") {
         this.state = "start";
         this.interval = setInterval(this.incrementTime, 1000);
-      }
-      else if (this.state === "start"){
+
+        this.startButton = require("../assets/pause.svg");
+      } else if (this.state === "start") {
         this.state = "pause";
         clearInterval(this.interval);
+
+        this.startButton = require("../assets/play.svg");
       }
     },
     stop() {
       this.state = "stop";
       clearInterval(this.interval);
       this.time = 0;
-    }
+    },
   },
 };
 </script>
