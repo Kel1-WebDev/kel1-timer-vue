03c887a8d986f02abe014de5df1196821b879d8b
Hargi <hargimuhammad@gmail.com>
2021-10-25 20:57:21 +0700

    feature(timer) : connect timer to database

--- front-end/src/components/StopwatchList.vue
+++ front-end/src/components/StopwatchList.vue
@@ -47,6 +47,7 @@
 
 <script>
 import StopwatchItem from "./StopwatchItem.vue";
+import axios from "axios";
 
 export default {
   name: "StopwatchList",
@@ -63,15 +64,20 @@ export default {
   methods: {
     addStopwatch() {
       var name = this.stopwatchName;
-      var stopwatch = {
-        id: this.lastId,
-        timer_name: name,
-        time: 0,
-        state: "stop",
-      };
-
-      this.stopwatchLists.push(stopwatch);
-      this.lastId++;
+      axios.post('http://localhost:3000/timer', { timer_name: name })
+        .then((response) => {
+          console.log(response);
+          var stopwatch = {
+            id: response.data.id,
+            timer_name: name,
+            time: 0,
+            state: "stop",
+          };
+          this.stopwatchLists.push(stopwatch);
+        })
+        .error((err) => {
+          console.error(err);
+        })
     },
     searchTimer(id) {
       for (let i = 0; i < this.stopwatchLists.length; i++) {
